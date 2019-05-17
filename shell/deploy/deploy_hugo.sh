6acd7c12be47cc2b64799c46dba23ca37f7b555f
Cloud Strife <cloudfstrife@sina.cn>
2019-05-17 20:40:20 +0800

    feat: add deploy hugo page

---
+++ shell/deploy/deploy_hugo.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Hugo 源代码git地址
+SOURCE_REPOSITORY=https://github.com/cloudfstrife/Hermes.git
+# Hugo 源代码目录
+SOURCE_FOLDER=/data/github/Hermes
+#Hugo 可执行程序位置
+HUGO_BIN=/usr/local/bin/hugo
+# 静态文件部署位置
+DEPLOY_FOLDER=/data/www
+
+# -------------------------------------------------------------------------------
+#  处理错误
+#  参数1 上一命令返回状态，一般为$?
+#  参数2 任务说明
+# -------------------------------------------------------------------------------
+function ShowError(){
+    if [ $1 != "0" ]; then
+        Log "ERROR" "$2"
+        exit 1
+    fi 
+}
+
+# -------------------------------------------------------------------------------
+#  日志输出
+#  参数1 日志级别
+#  参数2 日志内容
+# -------------------------------------------------------------------------------
+function Log(){
+    printf "%s [ %-7s ] : %s\n" "$(date '+%F %T')" "$1" "$2"
+}
+
+
+
+Fetch(){
+	if [ ! -d ${SOURCE_FOLDER} ]; then 
+		mkdir -p ${SOURCE_FOLDER}
+        git clone ${SOURCE_REPOSITORY} ${SOURCE_FOLDER}
+        cd ${SOURCE_FOLDER}
+        git submodule update --init --recursive
+	else
+        cd ${SOURCE_FOLDER}
+        git pull origin 
+    fi
+}
+
+Compile(){
+    rm -rf ${SOURCE_FOLDER}/public 
+    cd ${SOURCE_FOLDER}
+    hugo
+}
+
+Deploy(){
+    if [ ! -d ${DEPLOY_FOLDER} ]; then 
+		mkdir -p ${DEPLOY_FOLDER}
+    fi
+    rm -rf ${DEPLOY_FOLDER}/*
+    cp -r ${SOURCE_FOLDER}/public/* ${DEPLOY_FOLDER}
+}
+
+Main(){
+    Fetch
+    ShowError $? "Fetch "
+    Compile
+    ShowError $? "Compile "
+    Deploy
+    ShowError $? "Deploy "
+}
+
+Main
