9e64bb8f7e2b8888110beb8712c0d4e1602ae161
Cloud Strife <cloudfstrife@sina.cn>
2019-05-30 17:32:10 +0800

    feat:add generate golang model

--- shell/mysql/mysql.sql
+++ shell/mysql/mysql.sql
@@ -2,7 +2,6 @@
 ###########################################################################################################################
 ####                                             查询删除MySQL所有表的语句                                             ####
 ###########################################################################################################################
-
 SELECT 
     CONCAT('drop table `',
             TABLE_SCHEMA,
@@ -17,7 +16,6 @@ WHERE
 #############################################################################################################################
 ####                                             查询删除MySQL所有外键的语句                                             ####
 #############################################################################################################################
-
 SELECT 
     CONCAT('alter table `',
             TABLE_NAME,
@@ -62,72 +60,112 @@ FROM
 WHERE
     TABLE_SCHEMA = 'database_name';
 
+
+##############################################################################################################################
+####                                                     下划线转驼峰                                                     ####
+##############################################################################################################################
+DROP FUNCTION IF EXISTS UnderscoreToCamelCase;
+
+DELIMITER $$
+CREATE FUNCTION UnderscoreToCamelCase(content VARCHAR(100)) RETURNS VARCHAR(100) READS SQL DATA 
+BEGIN
+DECLARE POSITION_UNDER INT DEFAULT 0;
+DECLARE V VARCHAR(100) DEFAULT '';
+SELECT POSITION('_' in content) INTO POSITION_UNDER;
+WHILE POSITION_UNDER>0 DO 
+	SELECT concat(V,upper(SUBSTR(content,1,1)),SUBSTR(content,2,POSITION_UNDER-2)) into V;
+    SELECT SUBSTR(content,POSITION_UNDER+1,length(content)) into content;
+    SELECT POSITION('_' in content) INTO POSITION_UNDER;
+END WHILE ; 
+SELECT concat(V,upper(SUBSTR(content,1,1)),SUBSTR(content,2,length(content))) into V;
+RETURN V ;
+END
+$$
+DELIMITER ;
+
 ##############################################################################################################################
 ####                                                  生成Java实体类属性                                                  ####
 ##############################################################################################################################
-SELECT
-    CONCAT_WS(
-        ' ',
-        'private',
-        CASE DATA_TYPE
-    WHEN 'varchar' THEN
-        'String'
-    WHEN 'bigint' THEN
-        'long'
-    WHEN 'longtext' THEN
-        'String'
-    WHEN 'datetime' THEN
-        'Date'
-    WHEN 'int' THEN
-        'int'
-    WHEN 'decimal' THEN
-        'BigDecimal'
-    WHEN 'double' THEN
-        'double'
-    WHEN 'timestamp' THEN
-        'Timestamp'
-    WHEN 'longblob' THEN
-        'byte[]'
-    WHEN 'tinyint' THEN
-        'int'
-    WHEN 'text' THEN
-        'String'
-    WHEN 'char' THEN
-        'char'
-    WHEN 'date' THEN
-        'Date'
-    WHEN 'float' THEN
-        'float'
-    WHEN 'varbinary' THEN
-        'byte[]'
-    WHEN 'mediumtext' THEN
-        'String'
-    WHEN 'enum' THEN
-        'String'
-    WHEN 'blob' THEN
-        'byte[]'
-    WHEN 'set' THEN
-        'String'
-    WHEN 'time' THEN
-        'Time'
-    WHEN 'smallint' THEN
-        'int'
-    WHEN 'tinytext' THEN
-        'String'
-    WHEN 'binary' THEN
-        'byte[]'
-    WHEN 'bit' THEN
-        'boolean'
-    END,
-    column_name,
-    ';//',
-    COLUMN_COMMENT
-    ) as j 
+SELECT 
+    CONCAT_WS(' ',
+            'private',
+            CASE DATA_TYPE
+                WHEN 'varchar' THEN 'String'
+                WHEN 'bigint' THEN 'long'
+                WHEN 'longtext' THEN 'String'
+                WHEN 'datetime' THEN 'Date'
+                WHEN 'int' THEN 'int'
+                WHEN 'decimal' THEN 'BigDecimal'
+                WHEN 'double' THEN 'double'
+                WHEN 'timestamp' THEN 'Timestamp'
+                WHEN 'longblob' THEN 'byte[]'
+                WHEN 'tinyint' THEN 'int'
+                WHEN 'text' THEN 'String'
+                WHEN 'char' THEN 'char'
+                WHEN 'date' THEN 'Date'
+                WHEN 'float' THEN 'float'
+                WHEN 'varbinary' THEN 'byte[]'
+                WHEN 'mediumtext' THEN 'String'
+                WHEN 'enum' THEN 'String'
+                WHEN 'blob' THEN 'byte[]'
+                WHEN 'set' THEN 'String'
+                WHEN 'time' THEN 'Time'
+                WHEN 'smallint' THEN 'int'
+                WHEN 'tinytext' THEN 'String'
+                WHEN 'binary' THEN 'byte[]'
+                WHEN 'bit' THEN 'boolean'
+            END,
+            column_name,
+            ';//',
+            COLUMN_COMMENT) AS j
 FROM
     information_schema.`COLUMNS`
 WHERE
     TABLE_SCHEMA = 'database_name'
-AND TABLE_NAME = 'table_name'; 
+        AND TABLE_NAME = 'table_name'; 
+
+##############################################################################################################################
+####                                                  生成Java实体类属性                                                  ####
+##############################################################################################################################
+SELECT 
+    CONCAT(' ',
+            UNDERSCORETOCAMELCASE(column_name),
+            ' ',
+            CASE DATA_TYPE
+                WHEN 'varchar' THEN 'string'
+                WHEN 'bigint' THEN 'int64'
+                WHEN 'longtext' THEN 'string'
+                WHEN 'datetime' THEN 'time.Time'
+                WHEN 'int' THEN 'int32'
+                WHEN 'decimal' THEN 'big.Decimal'
+                WHEN 'double' THEN 'float64'
+                WHEN 'timestamp' THEN 'time.Time'
+                WHEN 'longblob' THEN 'byte[]'
+                WHEN 'tinyint' THEN 'int8'
+                WHEN 'text' THEN 'string'
+                WHEN 'char' THEN 'rune'
+                WHEN 'date' THEN 'time.Time'
+                WHEN 'float' THEN 'float32'
+                WHEN 'varbinary' THEN 'byte[]'
+                WHEN 'mediumtext' THEN 'string'
+                WHEN 'enum' THEN 'int'
+                WHEN 'blob' THEN 'byte[]'
+                WHEN 'set' THEN 'string'
+                WHEN 'time' THEN 'time.Time'
+                WHEN 'smallint' THEN 'int16'
+                WHEN 'tinytext' THEN 'string'
+                WHEN 'binary' THEN 'byte[]'
+                WHEN 'bit' THEN 'bool'
+            END,
+            '`json:"',
+            column_name,
+            '"`;//',
+            COLUMN_COMMENT) AS j
+FROM
+    information_schema.`COLUMNS`
+WHERE
+    TABLE_SCHEMA = 'database_name'
+        AND TABLE_NAME = 'table_name'; 
 
 ##############################################################################################################################
 ####                                                      查询库大小                                                      ####
